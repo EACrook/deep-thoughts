4d3b4b5a1222ba98f280dcfdf24fae8930072f3e
Emily Crook <Emily.A.Crook96@gmail.com>
2021-07-15 19:29:56 -0500

    mutations added with functionalities for adding user, login, add Thought, and add Reaction

--- server/server.js
+++ server/server.js
@@ -3,21 +3,26 @@ const express = require('express');
 const {
   ApolloServer
 } = require('apollo-server-express');
+const path = require('path');
 
 // import our typeDefs and resolvers
 const {
   typeDefs,
   resolvers
 } = require('./schemas');
+// import middleware
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // create a new Apollo server and pass in our schema data
 async function startApolloServer() {
   const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: authMiddleware
   });
   await server.start()
 
